97c5d95444946ddd669967352acf4c5c7bb30e0f
nzrsh <thelithium02@gmail.com>
2024-09-20 16:31:39 +0300

    teacher data deleted

--- routes/routes.go
+++ routes/routes.go
@@ -6,40 +6,52 @@ import (
 )
 
 func SetupRoutes(router *httprouter.Router) {
-	// Маршрут для главной страницы
-	router.GET("/", handlers.Login)
-	// Маршрут для авторизации через форму
+
+	// API
+	router.POST("/schooltest/save", handlers.SchoolSave)
+
 	router.POST("/authenticate", handlers.Authenticate)
 
 	router.GET("/authenticate", handlers.AuthenticateRedir)
-	// Маршрут для страницы имени
-	router.GET("/schooltest/test1", handlers.SchoolTest1)
-	// Маршрут для второго теста в школе
-	router.GET("/schooltest/test2", handlers.SchoolTest2)
-	// Маршрут для страницы авторизации
-	router.GET("/schooltest/welcome", handlers.WelcomeSchool)
-	// Маршрут для записи данных со школы
-	router.POST("/schooltest/save", handlers.SchoolSave)
-	// Маршрут страницы окончания любого теста
-	router.GET("/end", handlers.End)
-
-	router.GET("/registration", handlers.Registration)
 
 	router.GET("/results", handlers.DownloadRes)
-
-	router.GET("/schooltest/name", handlers.SchoolName)
 
 	router.GET("/registration/regme", handlers.SendSchools)
 
 	router.POST("/registration/reguser", handlers.RegUser)
 
-	router.GET("/studenttest/welcome", handlers.WelcomeStudent)
-	router.GET("/studenttest/test1", handlers.StudentTest1)
-	router.GET("/studenttest/test2", handlers.StudentTest2)
-	router.POST("/studenttest/save", handlers.StudentSave)
-	router.GET("/studenttest/name", handlers.StudentName)
-	router.GET("/.well-known/acme-challenge/:filename", handlers.MyTlsFiles)
-	router.GET("/getres", handlers.GetRes)
-	router.GET("/techsupport", handlers.TechSupport)
 	router.POST("/techsupport/sendmessage", handlers.GetMessage)
+
+	router.GET("/getres", handlers.GetRes)
+
+	router.POST("/studenttest/save", handlers.StudentSave)
+
+	router.GET("/.well-known/acme-challenge/:filename", handlers.MyTlsFiles)
+	// API
+
+	// HTML
+	router.GET("/", handlers.Login)
+
+	router.GET("/techsupport", handlers.TechSupport)
+
+	router.GET("/registration", handlers.Registration)
+
+	router.GET("/schooltest/welcome", handlers.WelcomeSchool)
+
+	router.GET("/schooltest/name", handlers.SchoolName)
+
+	router.GET("/schooltest/test1", handlers.SchoolTest1)
+
+	router.GET("/schooltest/test2", handlers.SchoolTest2)
+
+	router.GET("/studenttest/welcome", handlers.WelcomeStudent)
+
+	router.GET("/studenttest/name", handlers.StudentName)
+
+	router.GET("/studenttest/test1", handlers.StudentTest1)
+
+	router.GET("/studenttest/test2", handlers.StudentTest2)
+
+	router.GET("/end", handlers.End)
+	// HTML
 }
